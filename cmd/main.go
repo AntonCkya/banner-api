445c9ce9934fca6e797692d093d751c1ead940ff
Anton Sinyukov <60886237+AntonCkya@users.noreply.github.com>
2024-04-11 22:53:38 +0300

    init

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,6 @@ import (
 )
 
 func main() {
-	os.Setenv("DB", "postgres://postgres:XD_120403_1000$@localhost:5432/banner?sslmode=disable")
 	ctx := context.Background()
 	conn := db.New()
 	http.ListenAndServe("localhost:8000", route.New(ctx, conn))
