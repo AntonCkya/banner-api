d81fa32c434abc33999694b4590acc84a9aabd73
AntonCkya <sinyukovanton@yandex.ru>
2024-04-12 15:57:10 +0300

    add cache

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"context"
 	"net/http"
-	"os"
 
 	"github.com/AntonCkya/banner-api/internal/db"
 	"github.com/AntonCkya/banner-api/internal/route"
 )
 
 func main() {
-	os.Setenv("DB", "postgres://postgres:XD_120403_1000$@localhost:5432/banner?sslmode=disable")
 	ctx := context.Background()
 	conn := db.New()
 	http.ListenAndServe("localhost:8000", route.New(ctx, conn))
--- internal/model/content.go
+++ internal/model/content.go
@@ -1,4 +1,14 @@
 package model
 
+import "encoding/json"
+
 // баннер представляет собой  JSON-документ неопределенной структуры
 type Content map[string]interface{}
+
+func (c Content) MarshalBinary() ([]byte, error) {
+	return json.Marshal(c)
+}
+
+func (c Content) UnmarshalBinary(data []byte) error {
+	return json.Unmarshal(data, &c)
+}
